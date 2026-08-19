e52b8749737dfcf9f0e3e643941b4c0d242404c8
Minaessam246 <minaessam1810@gmail.com>
2026-08-19 08:53:24 +0300

    first commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,18 +1,65 @@
 import { useState } from "react"
 
- 
- 
- export default function Home() {
-    const [x, setX] = useState(10)
+export default function Home() {
+  const [board, setBoard] = useState(Array(9).fill(""))
+  const [isX, setIsX] = useState(true )
 
-    function changeX(){
+  const lines = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+  let winner = ""
+  let winLine = []
+ for (let line of lines) {
+  const [a,b,c] = line
+  if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+    winner = board[a]
+    winLine = line
+    break
+  }
+}
+  const isDraw = !winner && board.every(cell => cell !== "")
 
-  setX(Math.ceil(Math.random()*10))
-       
-    }
-    return<>
-    <h2 className={x>5?"bg-dark text-white":"bg-success text-dark"}>Home {x}</h2>
-    <button onClick={  changeX }className=" btn btn-danger my-3 "> Random</button>
+  function handleClick(i) {
+    if (board[i] || winner) return
+    const newBoard = [...board]
+    newBoard[i] = isX ? "X" : "O"
+    setBoard(newBoard)
+    setIsX(!isX)
+  }
 
-    </>
+  function restart() {
+    setBoard(Array(9).fill(""))
+    setIsX(true)
+  }
+
+  return (
+    <div>
+  
+
+    
+      <div className=" bg-dark text-white text-center py-4 fs-1 fw-bold">
+  {winner ? winner + " is the winner!" : isDraw ? "It's a draw!" : "Next: " + (isX ? "X" : "O")}
+</div>
+
+<div className="container">
+        <div className="row row-cols-3">
+        {board.map((cell, i) => (
+          <button
+            key={i}
+            onClick={() => handleClick(i)}
+            className={`  fw-bold border-1 ${winLine.includes(i)?"bg-secondary":"bg-dark"} ${isDraw ? "text-warning" : winLine.includes(i) ? "text-success" : "text-white"}  border`}
+            style={{
+              height: "180px",
+              fontSize:"100px",
+            }}
+          >
+            {cell}
+          </button>
+        ))}
+      </div>
+</div>
+
+      <div className="text-center">
+        <button onClick={restart} className="btn btn-danger my-3">Restart</button>
+      </div>
+    </div>
+  )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,13 +6,14 @@ export default function Navbar() {
   return (
 <>
 <div className="d-flex justify-content-between bg-danger">
-<h2 className='text-white my-2 mx-3'>test</h2>
+<h2 className='text-white my-2 mx-3'>Ejada Tasks</h2>
 
 <ul className=' d-flex'>
-    <li className='list-unstyled'> <NavLink className='text-white m-2  text-decoration-none' to="home">Home</NavLink> </li>
+    <li className='list-unstyled' > <NavLink className='text-white m-2  text-decoration-none' to="landing">task-1</NavLink> </li>
+    <li className='list-unstyled'> <NavLink className='text-white m-2  text-decoration-none' to="home">task-2</NavLink> </li>
     <li className='list-unstyled' > <NavLink className='text-white m-2  text-decoration-none' to="parent">Parent</NavLink> </li>
     <li className='list-unstyled' > <NavLink className='text-white m-2  text-decoration-none' to="gallery">Gallery</NavLink> </li>
-    <li className='list-unstyled' > <NavLink className='text-white m-2  text-decoration-none' to="landing">landing</NavLink> </li>
+  
   
   
 </ul>
